56619e7508c59490f41e58df7c49453b3a510bd5
Colin Misbach <colin@misbach.com>
2018-04-24 12:27:21 -0600

    Garbage changes, need to redo everything

--- N-Queens Temp/Program.cs
+++ N-Queens Temp/Program.cs
@@ -6,9 +6,31 @@ using System.Threading.Tasks;
 
 namespace N_Queens_Temp
 {
+    class Solution
+    {
+        public Solution(int boardSize)
+        {
+            steps = 0;
+            column = 0;
+            gameBoard = new int[boardSize];
+        }
+
+        public int CurrentRow()
+        {
+            return gameBoard[column];
+        }
+
+        public int steps;
+        public int column;
+        public int[] gameBoard;
+    }
+
     class Program
     {
-        static int[] gameBoard;
+
+
+        static Stack<Solution> solutions;
+        static int boardSize = 0;
 
         static void Main(string[] args)
         {
@@ -18,33 +40,102 @@ namespace N_Queens_Temp
 
         static void RestartAlgorithm()
         {
-            gameBoard = new int[GetBoardSizeFromUser()];
+            solutions = new Stack<Solution>();
+            boardSize = GetBoardSizeFromUser();
             PlaceAllQueens();
             PrintBoard();
         }
 
         static void PlaceAllQueens()
         {
-            bool queensToPlace = true;
+            solutions.Push(new Solution(boardSize));
 
-            while (queensToPlace)
+            Solution currentSolution = solutions.Peek();
+            bool hasSolution = true;
+
+            int row = 0;
+            while (hasSolution)
             {
+                bool placed = PlaceQueen(currentSolution.column, row);
+                if (placed)
+                {
+                    currentSolution.column++;
+                    row = 0;
+                }
+                else
+                {
+                    currentSolution.column--;
+                    row = currentSolution.CurrentRow() + 1;
+                }
 
+                if (currentSolution.column >= currentSolution.gameBoard.Length)
+                {
+                    if (UniqueSolution(currentSolution))
+                    {
+                        hasSolution = false;
+                        //solutions.Push(new Solution(boardSize));
+                        currentSolution = solutions.Peek();
+                        row = 0;
+                    }
+                    else
+                    {
+                        solutions.Pop();
+                        hasSolution = false;
+                    }
+                }
+                else if (currentSolution.column < 0)
+                {
+                    solutions.Pop();
+
+                    hasSolution = false;
+                }
             }
         }
 
-        static bool PlaceQueen(int colum, int row)
+        static bool PlaceQueen(int column, int row)
         {
+            bool placedQueen = true;
+            Solution solution = solutions.Peek();
+            int[] gameBoard = solution.gameBoard;
+            solution.steps++;
+            
+            if (column >= gameBoard.Length || row >= gameBoard.Length)
+            {
+                return false;
+            }
+            gameBoard[column] = row;
+            bool validPlacement = QueenIsValid(row, column);
+            int currentRow = row;
+            while (!validPlacement)
+            {
+                if (currentRow >= gameBoard.Length)
+                {
+                    placedQueen = false;
+                    break;
+                }
+
+                solution.steps++;
+                currentRow++;
+                gameBoard[column] = currentRow;
+                validPlacement = QueenIsValid(currentRow, column);
+            }
+            if (placedQueen)
+                Console.WriteLine($"Placed queen at [{column}, {currentRow}]");
+
+            return placedQueen;
+        }
+
+        static bool QueenIsValid(int row, int column)
+        {
+            int[] gameBoard = solutions.Peek().gameBoard;
             bool isValid = true;
 
-            gameBoard[colum] = row;
-
-            if (colum > 0)
+            if (column > 0)
             {
-                for (int i = colum - 1; i >= 0; --i)
+                for (int i = column - 1; i >= 0; --i)
                 {
-                    int checkIndexBelow = (colum - i) + row;
-                    int checkIndexAbove = (colum - i) - row;
+                    int checkIndexBelow = (column - i) + row;
+                    int checkIndexAbove = (column - i) - row;
                     if (gameBoard[i] == row || gameBoard[i] == checkIndexAbove || gameBoard[i] == checkIndexAbove)
                     {
                         isValid = false;
@@ -54,6 +145,40 @@ namespace N_Queens_Temp
             }
 
             return isValid;
+        }
+
+        static bool UniqueSolution(Solution solution)
+        {
+            bool unique = true;
+
+            foreach (Solution s in solutions)
+            {
+                if (s != solution)
+                {
+                    if (EqualSolutions(s, solution))
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+
+            return unique;
+        }
+
+        static bool EqualSolutions(Solution s1, Solution s2)
+        {
+            bool equal = true;
+            for (int i = 0; i < s1.gameBoard.Length; ++i)
+            {
+                if (s1.gameBoard[i] != s2.gameBoard[i])
+                {
+                    equal = false;
+                    break;
+                }
+            }
+
+            return equal;
         }
 
         static int GetBoardSizeFromUser()
@@ -83,6 +208,7 @@ namespace N_Queens_Temp
 
         static void PrintBoard()
         {
+            int[] gameBoard = solutions.Peek().gameBoard;
             int size = gameBoard.Length;
             for (int i = 0; i < size * size; ++i)
             {
